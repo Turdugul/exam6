79887f3b0c8c6aff6a067a4fb35fad0ab8422365
Daniiar Oskonbaev <daniiar94@gmail.com>
2021-05-03 19:39:05 +0600

    first commit

---
+++ src/component/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>
+            &copy; Oskonbaev Library <br/>
+            email: oskonbaev@gmail.com <br/>
+            phone: 00996555888222
+            </p>
+        </footer>
+    );
+};
+
+export default Footer;
